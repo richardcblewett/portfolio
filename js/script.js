a40b68d753a3a18a9b29efdf584ec3ccb0e0ad6d
Richard Blewett <richardcblewett@gmail.com>
2021-11-12 12:07:12 -0500

    adjusted 'contact me' font size

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ function addCtText () {
     let ctText = "Contact Me";
     let d = document.getElementById("contact-text") ;
     let fw = "normal";
+    let fs = "1.1em";
     const pageWidth = window.innerWidth ;
     if (pageWidth <= 600) {
         ctText = ctText+":";
         fw = "bold" ;
+        fs = "1.25em";
     }
     d.style.fontWeight = fw;
+    d.style.fontSize = fs;  
     d.innerText = ctText;
     document.getElementById("contact-text") = d ;
 }
