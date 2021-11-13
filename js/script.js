6acceb26cf75a9c5927a3c26f30ebd651455be44
Richard Blewett <richardcblewett@gmail.com>
2021-11-13 12:11:44 -0500

    small spacing adjustment in the background code

--- js/script.js
+++ js/script.js
@@ -16,3 +16,11 @@ function addCtText () {
     d.innerText = ctText;
     document.getElementById("contact-text") = d ;
 }
+
+
+let m = document.getElementById('tagline');
+alert(m);
+m.onmouseover = addContent;
+function addContent () {
+    alert("working");
+}
